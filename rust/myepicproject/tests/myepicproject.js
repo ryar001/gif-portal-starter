81b44e1413676957dc36db7ffb961dec10f5fba8
ryar001 <57259642+ryar001@users.noreply.github.com>
2022-03-06 22:45:44 +0800

    2

--- rust/myepicproject/tests/myepicproject.js
+++ rust/myepicproject/tests/myepicproject.js
@@ -1,13 +1,30 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
 
-describe("myepicproject", () => {
-  // Configure the client to use the local cluster.
+const main = async()=>{
+  console.log("startingTest")
+
+  // this is to set the anchor net eg local, devnet or mainnet
+  // env() use the setting we set in solana config get, 
   anchor.setProvider(anchor.Provider.env());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  // anchor.workspace will auto compile the code in libs.rs and get it deployed in the provider given
+  // note structure and naming of the folder is very impt
+  const program = anchor.workspace.Myepicproject;
+
+  //
+  const tx = await program.rpc.startStuffOff();
+
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
