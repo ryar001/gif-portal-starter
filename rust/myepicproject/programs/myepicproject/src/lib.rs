abb526fffcc84feb59d312af16d0d49d82758f1d
ryar001 <57259642+ryar001@users.noreply.github.com>
2022-03-20 20:56:41 +0800

    section2

--- rust/myepicproject/programs/myepicproject/src/lib.rs
+++ rust/myepicproject/programs/myepicproject/src/lib.rs
@@ -7,10 +7,58 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod myepicproject {
   use super::*;
+  // input ctx of type Context<StartStuffOff>  and output ProgramResult
+  // ctx is main way for endpoints to connect to the accounts Struct
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
     Ok(())
   }
 }
 
+/*First we've got [account(init, payer = user, space = 9000)]. 
+All we're doing here is telling Solana how we want to initialize BaseAccount.
+Note, if after running your test below you get the error Transaction simulation failed: Error processing Instruction 0: custom program error: 0x64, you will need to change space = 9000 to space = 10000. If you look at these docs from anchor you can see that they define a simple program that declares space = 8 + 8 (eg, 8 kilobytes + 8 kilobytes). The more logic we add to our program, the more space it will take up!
+  - init will tell Solana to create a new account owned by our current program.
+  - payer = user tells our program who's paying for the account to be created. In this case, it's the user calling the function.
+  - We then say space = 9000 which will allocate 9000 bytes of space for our account. You can change this # if you wanted, but, 9000 bytes is enough for the program we'll be building here!
+*/
+// The Account type is used when an instruction 
+// is interested in the deserialized data of the account. 
+// Consider the following example where we set some data in an account: 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    // 
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    
+    // get user signature to verify wallet
+    #[account(mut)]
+    pub user: Signer<'info>,
+    // system Program is the program controlling the solana cluster
+    // one of the function it does is creating acct as down below
+    pub system_program: Program <'info, System>,
+}
+
+
+// Serialize and deserialise to binary format to store into account
+#[derive(Debug,Clone, AnchorSerialize,AnchorDeserialize)]
+pub struct ItemStruct {
+  pub gif_link: String,
+  pub user_address: Pubkey
+}
+
+
+// tell solana the type the of data the account is going to store
+// below we instruct our program to make an acct of 64 byte of unsigned int 
+// to store out gif in total_gif
+#[account]
+pub struct BaseAccount {
+  pub total_gifs: u64,
+  // add another variable to the account with Vec type
+  // storing data of ItemStruct which we will define above
+  // essentially a list or array
+  pub gif_list: Vec<ItemStruct>,
+}
--- rust/myepicproject/tests/myepicproject.js
+++ rust/myepicproject/tests/myepicproject.js
@@ -1,20 +1,54 @@
+// import
 const anchor = require('@project-serum/anchor');
 
-const main = async()=>{
-  console.log("startingTest")
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+// async func, allow use of the await for
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  // this is to set the anchor net eg local, devnet or mainnet
-  // env() use the setting we set in solana config get, 
-  anchor.setProvider(anchor.Provider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
-  // anchor.workspace will auto compile the code in libs.rs and get it deployed in the provider given
-  // note structure and naming of the folder is very impt
+  // set our program folder name that contai
   const program = anchor.workspace.Myepicproject;
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
 
-  //
-  const tx = await program.rpc.startStuffOff();
+  // Call start_stuff_off, to get the accounts info needed
+  // NOTE: the camelCase is used in the Solana cluster Program
+  // but our solana JSON RPC will take in camelCase and convert
+  // to similar variable in snake_case 
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  await program.rpc.addGif("https://giphy.com/gifs/sunday-babybluecat-cocofofo-TQ1dWh0EzpPGARJ3LO", {
+  accounts: {
+    baseAccount: baseAccount.publicKey,
+    user: provider.wallet.publicKey,
+    },
+
+  });
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  //access the gif_list on the account
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
