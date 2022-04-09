7be124c190bc5b4b3fc06212daca92bf2101f81d
ryar001 <57259642+ryar001@users.noreply.github.com>
2022-04-09 14:22:53 +0800

    Update lib.rs

--- rust/myepicproject/programs/myepicproject/src/lib.rs
+++ rust/myepicproject/programs/myepicproject/src/lib.rs
@@ -4,6 +4,11 @@ use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// basic anchor definition
+// #[program] is where the function for the logic of the program is written
+// #[derive(Accounts)] is where is put our structs
+// #[account] 
+
 #[program]
 pub mod myepicproject {
   use super::*;
@@ -14,6 +19,24 @@ pub mod myepicproject {
     let base_account = &mut ctx.accounts.base_account;
     // Initialize total_gifs.
     base_account.total_gifs = 0;
+    base_account.dice_result=0
+    Ok(())
+  }
+
+  // The function now accepts a gif_link param from the user. We also reference the user from the Context
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+	// Build the struct.
+    let item = ItemStruct {
+      gif_link: gif_link.to_string(),
+      user_address: *user.to_account_info().key,
+    };
+		
+	// Add it to the gif_list vector.
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
     Ok(())
   }
 }
@@ -41,6 +64,14 @@ pub struct StartStuffOff<'info> {
     // one of the function it does is creating acct as down below
     pub system_program: Program <'info, System>,
 }
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account : Account<'info,BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
 
 
 // Serialize and deserialise to binary format to store into account
@@ -61,4 +92,5 @@ pub struct BaseAccount {
   // storing data of ItemStruct which we will define above
   // essentially a list or array
   pub gif_list: Vec<ItemStruct>,
+  pub dice_result: u64,
 }
